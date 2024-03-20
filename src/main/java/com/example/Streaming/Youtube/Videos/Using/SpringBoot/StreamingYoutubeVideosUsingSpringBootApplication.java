de60584acbfc8622d3f744b113bacf316cd17880
Sarthak <sarthak10082001@gmail.com>
2024-03-20 11:45:34 +0530

    inital commit

---
+++ src/main/java/com/example/Streaming/Youtube/Videos/Using/SpringBoot/StreamingYoutubeVideosUsingSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.Streaming.Youtube.Videos.Using.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingYoutubeVideosUsingSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingYoutubeVideosUsingSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Streaming/Youtube/Videos/Using/SpringBoot/StreamingYoutubeVideosUsingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Streaming.Youtube.Videos.Using.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingYoutubeVideosUsingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
